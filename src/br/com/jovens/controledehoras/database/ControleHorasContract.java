88777e7ac2cb50a57e76fa0e3d3e7e36c24dd34b
alexbispo <alexbispo.artes@gmail.com>
2014-07-10 09:11:57 -0300

    Alterações: ControleHorasContract.java DataBaseHelper.java

--- src/br/com/jovens/controledehoras/database/ControleHorasContract.java
+++ src/br/com/jovens/controledehoras/database/ControleHorasContract.java
@@ -4,6 +4,23 @@ import android.provider.BaseColumns;
 
 public final class ControleHorasContract {
 	
+	public static final String TEXT_TYPE = "TEXT";
+	public static final String INTEGER_TYPE = "INTEGER";
+	public static final String NULL_TYPE = "NULL";
+	public static final String REAL_TYPE = "REAL";
+	public static final String BLOB_TYPE = "BLOB";	
+	public static final String COMMA_SEP = ",";
+	
+	public static final String SQL_CREATE_APONTAMENTO = 
+			"CREATE TABLE " + TabelaApontamento.TABLE_NAME + " (" +
+					TabelaApontamento._ID + INTEGER_TYPE + "PRIMARY KEY" + COMMA_SEP +
+					TabelaApontamento.COLUMN_NAME_DATA + TEXT_TYPE + COMMA_SEP +
+					TabelaApontamento.COLUMN_NAME_MINUTOS + INTEGER_TYPE + COMMA_SEP +
+			" )";
+	
+	public static final String SQL_DROP_APONTAMENTO = 
+			"DROP TABLE IF EXISTS " + TabelaApontamento.TABLE_NAME;
+	
 	public ControleHorasContract() {}
 	
 	public abstract class TabelaApontamento implements BaseColumns {
--- src/br/com/jovens/controledehoras/database/DataBaseHelper.java
+++ src/br/com/jovens/controledehoras/database/DataBaseHelper.java
@@ -1,31 +1,17 @@
 package br.com.jovens.controledehoras.database;
 
+import static br.com.jovens.controledehoras.database.ControleHorasContract.SQL_CREATE_APONTAMENTO;
+import static br.com.jovens.controledehoras.database.ControleHorasContract.SQL_DROP_APONTAMENTO;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import br.com.jovens.controledehoras.database.ControleHorasContract.TabelaApontamento;
 
 public class DataBaseHelper extends SQLiteOpenHelper {
 	
 	public static final int DATABASE_VERSION = 1;
 	public static final String DATABASE_NAME = "ControleHoras.db";
 	
-	private static final String TEXT_TYPE = "TEXT";
-	private static final String INTEGER_TYPE = "INTEGER";
-	private static final String NULL_TYPE = "NULL";
-	private static final String REAL_TYPE = "REAL";
-	private static final String BLOB_TYPE = "BLOB";	
-	private static final String COMMA_SEP = ",";
 	
-	private static final String SQL_CREATE_APONTAMENTO = 
-			"CREATE TABLE " + TabelaApontamento.TABLE_NAME + " (" +
-					TabelaApontamento._ID + INTEGER_TYPE + "PRIMARY KEY" + COMMA_SEP +
-					TabelaApontamento.COLUMN_NAME_DATA + TEXT_TYPE + COMMA_SEP +
-					TabelaApontamento.COLUMN_NAME_MINUTOS + INTEGER_TYPE + COMMA_SEP +
-			" )";
-	
-	private static final String SQL_DROP_APONTAMENTO = 
-			"DROP TABLE IF EXISTS " + TabelaApontamento.TABLE_NAME;
 	
 	
 	public DataBaseHelper(Context context) {
